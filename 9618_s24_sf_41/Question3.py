2a751eb1924098dbf5ef4f6091a0102d15240b5b
Yongama <dfeni@issa.co.za>
2024-09-19 14:08:03 +0200

    3 (d) (i) Complete

--- 9618_s24_sf_41/Question3.py
+++ 9618_s24_sf_41/Question3.py
@@ -26,43 +26,42 @@ def Enqueue(DataToInsert):
     QueueData[QueueTail] = DataToInsert
     return True
 
+Enqueue(12)
+Enqueue(45)
 
-def Dequeue():
-    global QueueHead
-    global QueueTail
-    global QueueData
-    
-    if QueueHead < 0:
+
+
+def DeQueue():
+    global QueueHead, QueueTail, QueueData
+    if QueueHead < 0 or QueueHead > QueueTail:
         return False
     else:
-        QueueHead = QueueHead +1
+        QueueHead = QueueHead + 1
+        return QueueData[QueueHead - 1]
     
-        return QueueData[QueueHead-1]
-
+# (d) (i)
 
 def StoreItems():
     
-    global QueueHead,QueueTail, QueueData
-    invalidCount = 0
+    global QueueData, QueueHead, QueueTail
+    InvalidCount = 0
     for i in range(10):
-        string = input("Enter the first string ")
-        total = int(string[0])*1 + int(string[1])*3 + int(string[2])*1+int(string[3])*3 + int(string[4])*1 + int(string[5])*3
-        total = int(total / 10) 
-        if total==int(string[6]) or tota
-    
-    
-    
-    
-    
-    
-    
-    
-    
-Enqueue(45)
-Enqueue(5)
-Enqueue(4)
+        string = input("Enter the string ")
+        total =  int(string[0])*1 + int(string[1])*3 +int(string[2])*1+int(string[3])*3+int(string[4])*1+int(string[5])*3
+        total = int(total /10)
+        if (total == 10 and string[6] == "X") or (total == int(string[6])):
+            result = Enqueue(string[:5])
+            if result == True:
+                print("The string was succesfully inserted! ")
+            else:
+                print("The Queue is full")
+        else:
+            InvalidCount += 1
+            
+    print("There were ", InvalidCount, " invalid items")
+            
 
-nextValue = Dequeue()
-print("The next value ", nextValue)
-
-
+            
+            
+                
+        
