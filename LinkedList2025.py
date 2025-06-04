3de14217b110f876a6fb8377a79f64b2880ae231
Yongama <dfeni@issa.co.za>
2025-06-04 11:05:23 +0200

    Complete Linked List in 2 1D arrays

--- LinkedList2025.py
+++ LinkedList2025.py
@@ -1,40 +1,87 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu May 29 14:51:25 2025
+Created on Fri May 30 09:57:50 2025
 
 @author: Support
 """
 
- #Python program for finding an item in a linked list
-myLinkedList = [27, 19, 36, 42, 16, None, None, None, None, None, None, None]
-myLinkedListPointers = [-1, 0, 1, 2, 3 ,6 ,7 ,8 ,9 ,10 ,11, -1]
-startPointer = 4
+# Simulated linked list using arrays
+myLinkedList = [None] * 12
+myLinkedListPointers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, -1]
+startPointer = -1
 nullPointer = -1
+heapStartPointer = 0
+
 def find(itemSearch): 
-   found = False
-   itemPointer = startPointer
-   while itemPointer != nullPointer and not found:
+    found = False
+    itemPointer = startPointer
+    while itemPointer != nullPointer and not found:
         if myLinkedList[itemPointer] == itemSearch:
-           found = True
+            found = True
         else:
-           itemPointer = myLinkedListPointers[itemPointer]
-   return itemPointer
-#enter item to search for
-item = int(input("Please enter item to be found "))
-result = find(item) 
+            itemPointer = myLinkedListPointers[itemPointer]
+    return itemPointer if found else -1
+
+def insert(itemAdd):
+    global startPointer, heapStartPointer
+    if heapStartPointer == nullPointer:
+        print("Linked List full")
+    else:
+        newNodePointer = heapStartPointer
+        heapStartPointer = myLinkedListPointers[heapStartPointer]
+        myLinkedList[newNodePointer] = itemAdd
+        myLinkedListPointers[newNodePointer] = startPointer
+        startPointer = newNodePointer
+
+def delete(itemDelete):
+    global startPointer, heapStartPointer
+    currentPointer = startPointer
+    previousPointer = nullPointer
+    found = False
+
+    while currentPointer != nullPointer and not found:
+        if myLinkedList[currentPointer] == itemDelete:
+            found = True
+        else:
+            previousPointer = currentPointer
+            currentPointer = myLinkedListPointers[currentPointer]
+
+    if not found:
+        print("Item not found in the list.")
+        return
+
+    # Remove node from the linked list
+    if previousPointer == nullPointer:
+        # Deleting the first node
+        startPointer = myLinkedListPointers[currentPointer]
+    else:
+        myLinkedListPointers[previousPointer] = myLinkedListPointers[currentPointer]
+
+    # Add the node back to the free list
+    myLinkedListPointers[currentPointer] = heapStartPointer
+    heapStartPointer = currentPointer
+    myLinkedList[currentPointer] = None
+    print(f"Item {itemDelete} deleted.")
+
+# --- Example usage ---
+insert(10)
+insert(20)
+insert(30)
+
+print("Initial list:")
+print(myLinkedList)
+print(myLinkedListPointers)
+
+item = int(input("Enter item to delete: "))
+delete(item)
+
+print("List after deletion:")
+print(myLinkedList)
+print(myLinkedListPointers)
+
+searchItem = int(input("Enter item to find: "))
+result = find(searchItem)
 if result != -1:
-    print("Item found")
+    print(f"Item found at index {result}")
 else:
     print("Item not found")
-    
-    
-def insert(itemAdd):
-    global startPointer
-    if heapStartPointer == nullPointer:
-       print("Linked List full")
-    else:
-       tempPointer = startPointer
-       startPointer = heapStartPointer
-       heapStartPointer = myLinkedListPointers[heapStartPointer]
-       myLinkedList[startPointer] = itemAdd
-       myLinkedListPointers[startPointer] = tempPointer
